3662ea40b038819f6a3b4e3f099a36ca2fecb590
Gajanan Chaudhari <gajanan77chaudhari@gmail.com>
2025-07-14 14:09:58 +0000

    remove the dolor sine in Total Income $0 for July
    Total Expenses
    $0
    for

--- src/components/monthly-overview.tsx
+++ src/components/monthly-overview.tsx
@@ -29,9 +29,9 @@ export function MonthlyOverview({ transactions, currentDate }: MonthlyOverviewPr
   const netProfit = totalIncome - totalExpenses;
 
   const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('en-IN', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'INR',
       maximumFractionDigits: 0,
     }).format(amount);
   };
